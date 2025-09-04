7ce1375aedee9309c07ca60afc066edcd48fc7b3
Da-met <dmetelskaya@yandex.ru>
2025-09-04 13:25:32 +0300

    Fix 05

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,9 @@
 
 
 
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
 
 // export default ({ mode }) => {
 //   const env = loadEnv(mode, process.cwd(), '');
@@ -53,9 +53,16 @@ import svgr from 'vite-plugin-svgr';
 //   });
 // };
 
+
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const apiURL = env.VITE_API_URL;
+  const apiURL = env.VITE_API_URL || 'http://localhost:5000/api';
+
+  console.log('__API__:', apiURL); // временно, чтобы проверить, что prod берет правильный URL
 
   return defineConfig({
     plugins: [svgr({ exportAsDefault: true }), react()],
@@ -63,6 +70,5 @@ export default ({ mode }) => {
     define: {
       __API__: JSON.stringify(apiURL),
     },
-    esbuild: { target: 'es2018' },
   });
 };
