a88ed5fc74bd0227642e8be5a1ad01876d872c8c
Da-met <dmetelskaya@yandex.ru>
2025-09-04 12:40:59 +0300

    Fix 03

--- vite.config.ts
+++ vite.config.ts
@@ -29,27 +29,40 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
+// export default ({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '');
+//   const viteApi = env.VITE_API_URL; // из .env или .env.production
+//   const fallback = 'http://localhost:5000/api';
+
+
+//   console.log('VITE_API_URL env:', viteApi);
+//   console.log('__API__ will be:', process.env.API_URL || viteApi || fallback);
+
+
+//   return defineConfig({
+//     plugins: [svgr({ exportAsDefault: true }), react()],
+//     resolve: { alias: [{ find: '@', replacement: '/src' }] },
+//     define: {
+//       __IS_DEV__: JSON.stringify(mode !== 'production'),
+//       // При сборке Vite подставит process.env.API_URL (если есть у Vercel),
+//       // иначе VITE_API_URL, иначе localhost.
+//       __API__: JSON.stringify(process.env.API_URL || viteApi || fallback),
+//       __PROJECT__: JSON.stringify('frontend'),
+//     },
+//     esbuild: { target: 'es2018' },
+//   });
+// };
+
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const viteApi = env.VITE_API_URL; // из .env или .env.production
-  const fallback = 'http://localhost:5000/api';
-
-
-  console.log('VITE_API_URL env:', viteApi);
-  console.log('__API__ will be:', process.env.API_URL || viteApi || fallback);
-
+  const apiURL = env.VITE_API_URL;
 
   return defineConfig({
-    plugins: [svgr({ exportAsDefault: true }), react()],
-    resolve: { alias: [{ find: '@', replacement: '/src' }] },
     define: {
-      __IS_DEV__: JSON.stringify(mode !== 'production'),
-      // При сборке Vite подставит process.env.API_URL (если есть у Vercel),
-      // иначе VITE_API_URL, иначе localhost.
-      __API__: JSON.stringify(process.env.API_URL || viteApi || fallback),
-      __PROJECT__: JSON.stringify('frontend'),
+      __API__: JSON.stringify(apiURL),
     },
+    plugins: [react(), svgr()],
+    resolve: { alias: [{ find: '@', replacement: '/src' }] },
     esbuild: { target: 'es2018' },
   });
 };
-
