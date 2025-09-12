df53219cc91095393fbf6b557e389a80d5e42e6a
Da-met <dmetelskaya@yandex.ru>
2025-09-12 19:23:18 +0300

    fixed the font and scroll 03

--- src/features/fetchWishesByListId/ui/WishesByList.tsx
+++ src/features/fetchWishesByListId/ui/WishesByList.tsx
@@ -12,6 +12,8 @@ import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch
 
 import CircleIcon from '@/shared/assets/icons/circle.svg'
 import CheckIcon from '@/shared/assets/icons/check.svg'
+import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
+import { Wish } from '@/entities/Wish';
 
 
 
@@ -24,6 +26,7 @@ export const WishesByList = ({ listId, className }: WishesByListProps) => {
     const dispatch = useAppDispatch();
     const wishes = useSelector(getWishesFromList);
     const isLoading = useSelector(getWishesByListLoading);
+    // const isLoading = true;
     const error = useSelector(getWishesByListError);
 
     useEffect(() => {
@@ -32,21 +35,56 @@ export const WishesByList = ({ listId, className }: WishesByListProps) => {
         }
     }, [listId, dispatch]);
 
-    if (isLoading) return <div className={cls.empty}>Загрузка подарков...</div>;
+    // if (isLoading) return <div className={cls.empty}>Загрузка подарков...</div>;
     if (error) return <div className={cls.empty}>Ошибка: {error}</div>;
     if (!wishes.length) return <div className={cls.empty}>Подарков пока нет</div>;
 
-    return (
-        <div className={cls.WishesByList}>
-        {wishes.map((wish) => (
-            <div className={cls.wrap}>
-                <WishListItem key={wish.id} wish={wish} />
-                {wish?.completed && <div className={cls.pin}><CheckIcon/></div>}
-                {!wish?.completed && wish?.isReserved && <div className={cls.pin}><CircleIcon/></div>}
-            </div>
-            
-        ))}
-        
+
+    const renderWish = (wish: Wish) => (
+        <div className={cls.wrap}>
+            <WishListItem
+                wish={wish}
+                className={cls.card}
+                key={wish.id}
+            />
+            {wish?.completed && <div className={cls.pin}><CheckIcon/></div>}
+            {!wish?.completed && wish?.isReserved && <div className={cls.pin}><CircleIcon/></div>}
         </div>
+    );
+
+
+
+    return (
+        
+            <>
+            {isLoading ? 
+                <div className={cls.skeleton}>
+                    <div className={cls.wrapperSceleton}>
+                        <Skeleton className={cls.sceletonLi}/>
+                        <Skeleton className={cls.sceletonLi}/>
+                        <Skeleton className={cls.sceletonLi}/>
+                    </div>
+                </div> 
+                : 
+                <div className={cls.WishesByList}>
+                    <div className={cls.WishList}>
+                        <div className={cls.wrapper}>
+                            {wishes.length > 0
+                                ? wishes.map(renderWish)
+                                : null
+                            }
+                        </div>
+                    </div>
+                </div> 
+            }
+            {/* {wishes.map((wish) => (
+                <div className={cls.wrap}>
+                    <WishListItem key={wish.id} wish={wish} />
+                    {wish?.completed && <div className={cls.pin}><CheckIcon/></div>}
+                    {!wish?.completed && wish?.isReserved && <div className={cls.pin}><CircleIcon/></div>}
+                </div>
+            ))} */}
+            </>
+        
     );
 };
